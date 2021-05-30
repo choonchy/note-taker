10378de35ef294fe9c60b417cc97c0746aebac4d
Tom Chappell <choonchy@Toms-MacBook-Pro.local>
2021-05-30 18:39:58 +1000

    Final code changes

--- public/assets/js/server.js
+++ public/assets/js/server.js
@@ -1,6 +1,30 @@
-const express = require('express')
-var PORT = process.env.PORT || 3001;
+const express = require('express');
+const path = require('path');
+const db = require('../../../db/db.json');
+const fs = require('fs');
+const shortid = require('shortid')
+var PORT = 3001
 
-const app = express()
+const app = express();
 
-app.get('/public/notes.html')
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../../index.html')))
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '../../notes.html')));
+app.get('/api/notes', (req, res) => res.json(db))
+app.post('/api/notes', (req, res) => {
+    let note = {
+        title: req.body.title,
+        text: req.body.text,
+        id: shortid.generate()
+    }
+    db.push(note);
+    fs.writeFileSync('../../../db/db.json', JSON.stringify(db), (err) => {
+        if (err) throw err
+        return res.json(db);
+    });
+});
+
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
